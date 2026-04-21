b3703f9e552023db9944b35ad8298b336d7644f4
ISH MALIK <55532361+ishmalik12@users.noreply.github.com>
2026-04-21 22:05:54 +0530

    app.py-clear

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
